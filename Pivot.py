e0cd9d0b5e3e4fb158881fdc28f497899628e802
LucSan10 <31455488+LucSan10@users.noreply.github.com>
2018-03-31 04:47:29 -0300

    Fixed bugs

--- Pivot.py
+++ Pivot.py
@@ -1,16 +1,15 @@
 import numpy as np
 
 def pivot(A, index1, rep):
-	print(A.shape[0])
+	P = np.eye(A.shape[0], A.shape[1])
+	error = 0
 	if rep == A.shape[0]:
 		error = 1
-		return (A,error)
+		return (P,error)
 	index2 = index1+rep
 
-	if A.shape[0] <= index2: index2 -= A.shape(0)
+	if A.shape[0] <= index2: index2 -= A.shape[0]
 	
 	if not (A[index2,index1]): (P,error) = pivot(A, index1, rep+1)
-	else:
-		P = np.eye(A.shape[0], A.shape[1])
-		P[[index1,index2]] = P[[index2,index1]]
-		return(P, error)
+	else: P[[index1,index2]] = P[[index2,index1]]
+	return(P, error)
