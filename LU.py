2fbf6fadc5a3ec341b7ff26c50e9ec48ccfec6b0
LucSan10 <31455488+LucSan10@users.noreply.github.com>
2018-03-31 04:46:57 -0300

    Lacking singular matrix test-case

--- LU.py
+++ LU.py
@@ -1,20 +1,19 @@
 import numpy as np
 import GaussJordan as gj
-import Pivot as piv
+import Pivot as p
 
 def LUDecomp(A):
 	size = A.shape
-	print(size)
 	P = np.eye(size[0], size[1], dtype = float)
 	L = np.copy(P)
 	U = np.copy(A)
-	error = False
-
+	error = 0
 	for i in range(size[0]-1):
 		if not (U[i,i]):
-			(tempP,error) = piv.pivot(U, i, 1)
+			(tempP,error) = p.pivot(U, i, 1)
 			U = tempP.dot(U)
 			P = tempP.dot(P)
+			L = P.dot(L.dot(tempP))
 		if error:
 			print("the matrix\n"+str(A.view())+"\ncannot be inverted")
 			return
@@ -24,6 +23,15 @@ def LUDecomp(A):
 
 A = np.array([[1,2,4],[3,8,14],[2,6,13]], dtype = float)
 B = np.array([[3,1],[4,2]], dtype = float)
-(P,L,U) = LUDecomp(B)
+C = np.array([[3,1],[-6,-4]], dtype = float)
+D = np.array([[3,1,6],[-6,0,-16],[0,8,-17]], dtype = float)
+E = np.array([[2,1,0,1],[2,1,2,3],[0,0,1,2],[-4,-1,0,2]])
 
-print(str(P)+"\n\n"+str(L)+"\n\n"+str(U))
+(P,L,U) = LUDecomp(E)
+
+print("A = PLU\nA is:\n" + str(E))
+print("\nP equals:\n" + str(P))
+print("\nL equals:\n" + str(L))
+print("\nU equals:\n" + str(U))
+
+print("\nProof:\n" + str(P.dot(L.dot(U))))
