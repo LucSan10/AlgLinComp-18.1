5ab840bce28ac52d51a1fbfc97a2a88a8467e9f1
gabrielmmoraes <gabrielm.moraes@hotmail.com>
2018-04-01 23:26:05 -0300

    Cleaning unnecessary variables and print functions used for debbuging

--- Cholesky.py
+++ Cholesky.py
@@ -7,21 +7,10 @@ def choleskyDecomp(matrix):
 	L = np.zeros((size[0], size[1]), dtype = float)
 
 	for i in range(1, n+1):
-		temp1 = matrix[i-1][i-1]
-		temp2 = choleskySum(i, i, L)
-		L[i-1][i-1] = np.sqrt(temp1 - temp2)
-		#print("linha " + str(i) + " coluna " + str(i) + "-> " + str(temp1) + " - " + str(temp2) + " = " + str(L[i-1][i-1]) + "^2")
-		#print(str(L) + "\n")
-		
-		for j in range (i+1, n+1):
-			temp1 = matrix[j-1][i-1]
-			temp2 = choleskySum(j, i, L)
-			temp3 = L[i-1][i-1]
-			temp4 = (1 / temp3) * (temp1 - temp2)
-			L[j-1][i-1] = temp4
-			#print("linha " + str(j) + " coluna " + str(i) + "-> " + "1 / " + str(temp3) + " * ("  + str(temp1) + " - " + str(temp2) + ") = " + str(L[j-1][i-1]))
-			#print(str(L) + "\n")
+		L[i-1][i-1] = np.sqrt(matrix[i-1][i-1] - choleskySum(i, i, L))
 
+		for j in range (i+1, n+1):
+			L[j-1][i-1] = (1 / L[i-1][i-1]) * (matrix[j-1][i-1] -choleskySum(j, i, L))
 	LT = np.transpose(L)
 
 	return (L,LT)
@@ -32,10 +21,6 @@ def choleskySum(lineIndex, columnIndex, L):
 		return temp
 	for i in range(0, columnIndex - 1):
 		temp += L[lineIndex-1][i] * L[columnIndex-1][i]
-		#print(L[lineIndex-1][i])
-		#print(L[columnIndex-1][i])
-
-	#print (str(temp) + " soma")
 	return temp
 
 A = np.array([[1,0.2,0.4],[0.2,1,0.5],[0.4,0.5,1]], dtype = float)
@@ -44,9 +29,9 @@ C = np.array([[18,22,54,42],[22,70,86,62],[54,86,174,134],[42,62,134,106]], dtyp
 
 
 
-(L,LT) = choleskyDecomp(C)
+(L,LT) = choleskyDecomp(A)
 
-print("A = L * LT\nA defined as:\n" +str(C))
+print("A = L * LT\nA defined as:\n" +str(A))
 print("\nL equals:\n" + str(L))
 print("\nLT equals:\n" + str(LT))
 print("\nL * LT equals:\n" + str(L.dot(LT)))
