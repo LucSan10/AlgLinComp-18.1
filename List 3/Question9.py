ed2f4a89092c30a59839606b159b2ec8cd2a8326
gabrielmmoraes <gabrielm.moraes@hotmail.com>
2018-04-25 22:00:29 -0300

    Tá acabando

---
+++ List 3/Question9.py
@@ -0,0 +1,18 @@
+import LeastSq as ls
+import numpy as np
+
+n = input("Please define the number of points:  ")
+
+arr = []
+
+for i in range(int(n)):
+    print("Please input an X value: ")
+    x = input("X: ")
+    print("Please input an Y value: ")
+    y = input("Y: ")
+
+    arr.append([float(x),float(y)])
+
+A = np.array(arr)
+
+print(A)
